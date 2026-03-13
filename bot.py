6495dd9b45d66c59b18edbef75abbe8ef3d6f5fc
WZNTG <maksi3mstepanov13@gmail.com>
2026-03-13 23:56:10 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-TOKEN = "8303496738:AAG5Kr64yBwGRZdUVxC4LCtYOSRvPT-BXPA"
+TOKEN = "8477161043:AAEmtwhIKd5wiYzQ0W5r8cuVyCx_t-FJuiM"
 ADMIN_ID = 5394084759
 
 logging.basicConfig(level=logging.INFO)
